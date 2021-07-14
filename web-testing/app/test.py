e7c7b010aa1dd8d0eadf3d16363840750da360cc
fanyCaz <stepheniecaz@gmail.com>
2021-07-14 14:10:03 -0500

    test final

--- web-testing/app/test.py
+++ web-testing/app/test.py
@@ -4,9 +4,7 @@ from selenium.webdriver.common.by import By
 
 driver = Chrome()
 driver.get("http://www.migala.mx")
-#Chrome(executable_path='opt/WebDriver/bin/chromedriver')
-"""
-findElements(By)
+print("Encontrando elementos comúnes en la página web")
 print( driver.current_url )
 print("Regresar")
 driver.back()
@@ -14,7 +12,6 @@ print("Hacia adelante")
 driver.forward()
 print("Refrescar la página")
 driver.refresh()
-"""
 print(f"Título: {driver.title}")
 # Vínculos de contacto
 print("Vínculos de contacto")
@@ -26,16 +23,48 @@ contact.find_element_by_id("menu-item-1554").click()
 driver.back()
 # Botón de búsqueda, se espera que obtenga un modal
 driver.find_element_by_id("cb-s-trigger-gen").click()
-search_box = driver.find_by_css_selector(".cb-search-field")
-# Vinculos que aparecen cuando se hace la búsqueda
+search_box = driver.find_element_by_css_selector(".cb-search-field")
 
-"""
-navbar = driver.find_element(By.ID, "cb-nav-bar")
-menu_items = navbar.find_elements_by_css_selector(".menu-item")
-print("Secciones del menú principal")
-print( menu_items)
-for item in menu_items:
-  print(item)
-"""
-#driver.switch_to.new_window("tab")
+driver.find_element_by_css_selector(".cb-close-m").click()
+
+# Vínculos de menú principal
+print("Vínculos de menú principal")
+driver.find_element_by_id("menu-item-1687").click()
+driver.back()
+driver.find_element_by_id("menu-item-1690").click()
+driver.back()
+driver.find_element_by_id("menu-item-1688").click()
+driver.back()
+driver.find_element_by_id("menu-item-1692").click()
+driver.back()
+driver.find_element_by_id("menu-item-1689").click()
+driver.back()
+driver.find_element_by_id("menu-item-1691").click()
+driver.back()
+
+# Vinculos de publicaciones
+driver.find_element_by_xpath('//a[contains(@href, "amasando")]')
+driver.find_element_by_xpath('//a[contains(@href, "panorama")]')
+driver.find_element_by_xpath('//a[contains(@href, "magia")]')
+driver.find_element_by_xpath('//a[contains(@href, "metamorfosis")]')
+driver.find_element_by_xpath('//a[contains(@href, "neoliberalismo")]')
+driver.find_element_by_xpath('//a[contains(@href, "jojos")]')
+driver.find_element_by_xpath('//a[contains(@href, "simbolismo")]')
+driver.find_element_by_xpath('//a[contains(@href, "casas")]')
+driver.find_element_by_xpath('//a[contains(@href, "matate")]')
+driver.find_element_by_xpath('//a[contains(@href, "pintura")]')
+
+try:
+  driver.find_element_by_css_selector(".dona")
+except:
+  print("Elemento no existe, pero debe existir")
+
+# En busqueda de la página huérfana
+try:
+  driver.find_element_by_xpath('//a[contains(@href,"tianguis")]')
+  print("La página puede encontrarse dentro del principal")
+except:
+  print("La página es huérfana")
+
+driver.get("http://tianguis.migala.mx")
 driver.quit()
